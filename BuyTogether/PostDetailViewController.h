ea904443810ca6de97b06f983631cad3b24d79ef
CocoaZeng <wangzeng.eleven@gmail.com>
2014-08-23 14:33:14 -0700

    login

---
+++ BuyTogether/PostDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  PostDetailViewController.h
+//  BuyTogether
+//
+//  Created by Zeng Wang on 8/23/14.
+//  Copyright (c) 2014 yuebai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostDetailViewController : UIViewController
+
+@end
