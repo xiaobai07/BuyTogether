c8abbc871d3df3a92e3961851963249f4945e9b4
CocoaZeng <wangzeng.eleven@gmail.com>
2014-08-23 22:17:59 -0700

    Add circle UI item

--- BuyTogether/FeedContributionStatusTableViewCell.h
+++ BuyTogether/FeedContributionStatusTableViewCell.h
@@ -11,5 +11,6 @@
 @interface FeedContributionStatusTableViewCell : UITableViewCell
 @property (weak, nonatomic) IBOutlet UILabel *minimalContribution;
 @property (weak, nonatomic) IBOutlet UITextField *contribution;
-
+@property (weak, nonatomic) IBOutlet UILabel *contributedAmount;
+@property (weak, nonatomic) IBOutlet UILabel *totalContribution;
 @end
